7e5ba77fbe7ea93f43886ec8996e94ac60e887d2
Mvrs <johnsonmarlon18@gmail.com>
2018-10-25 21:42:14 -0700

    Pushing Lab-4

--- src/App.js
+++ src/App.js
@@ -3,17 +3,62 @@ import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 import { doTest } from './redux/actions';
+import Header from './Header';
 
 class App extends Component {
+  state = {
+    color: 'black',
+    banner: 'hello',
+    isOpen: false,
+    title: ''
+  };
+
+  buttonHandler = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
+  textHandler = e => {
+    this.setState({
+      banner: e.target.value
+    });
+  };
+
+  // function to change the text of the button when clicked
+
+  handleClick = e => {
+    this.setState({
+      title: e.target.value,
+      isOpen: !this.state.isOpen
+    });
+  };
+
   render() {
+    const { isOpen } = this.state;
+
+    let name = <h2>Marlon</h2>;
+    let myBanner;
+    myBanner = this.state.isOpen ? (
+      <Header banner={this.state.banner} />
+    ) : (
+      myBanner
+    );
     return (
-      <div className="App">
+      <div className="App" onClick={this.handleClick}>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <h2>413 Starter Kit </h2>
+          <p>{this.props.test}</p>
+          {myBanner}
+          <input
+            style={{ margin: 10 }}
+            value={this.state.banner}
+            onChange={this.textHandler}
+          />
+          <br />
+          <button onClick={this.handleClick}>
+            {!isOpen ? 'Click Me!' : 'Clicked'}
+          </button>
         </header>
       </div>
     );
@@ -22,7 +67,7 @@ class App extends Component {
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    test: state.testReducer.test,
+    test: state.testReducer.test
   };
 };
 
---
+++ src/components/Part1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../App/App.css';
+
+const part1 = props => (
+  <div className="App">
+    <h1>{props.title}</h1>
+  </div>
+);
+
+export default part1;
